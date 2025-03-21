fed15ce5568345b3de59a2bd6a10538778dd5634
Monika Grzanek <grzanekmonika@o2.pl>
2025-03-21 12:07:54 +0100

    Update html code for contact form, update styles for isSent, update /contact/send-message endpoint

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.post('/contact/send-message', upload.single('picture'), (req, res) => {
     const picture = req.file;
 
     if(author && sender && title && picture && message) {
-      res.render('contact', { isSent: true, filename: req.file.originalname });
+      res.render('contact', { isSent: true, filename: picture.originalname, picturePath: `/uploads/${picture.filename}` });
     }
     else {
       res.render('contact', { isError: true });
